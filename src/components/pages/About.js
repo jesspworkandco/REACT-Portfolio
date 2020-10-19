484623bf89e3cece7ab67667fdaee776a8432ff9
Jessica Perez <1jessicaperez@gmail.com>
2020-10-19 18:51:55 -0400

    Dynamic topics added to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,71 +4,101 @@ import NavBar from "../NavBar";
 import Image from "react-bootstrap/Image";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import "./about.css";
 
-function About() {
-  return (
-    <>
-      <NavBar />
-      <div className="aboutbody">
-        <Container className="container">
-          <Row>
-            <Col md={4} className="first-column text-center">
-              <Image
-                className="french-cat"
-                src="https://i.pinimg.com/736x/e6/3f/94/e63f94393ad1765dbd4923e17c94f140--oui-oui-berets.jpg"
-                roundedCircle
-              />
-              <h4 className="contact mt-3">
-                NEW YORK, NY 10010
-                <br /> <span className="cell">(917) 847-1533</span>
-                <br />
-                <a
-                  data-toggle="tooltip"
-                  title="Send me a message!"
-                  id="email"
-                  href="mailto:1jessicaperez@gmail.com"
-                >
-                  1JESSICAPEREZ@GMAIL.COM
-                </a>
-              </h4>
-              {/* <img
-            src="https://i.pinimg.com/736x/e6/3f/94/e63f94393ad1765dbd4923e17c94f140--oui-oui-berets.jpg"
-            alt="french cat icon"
-            width="250"
-          ></img> */}
-            </Col>
-            <Col md={8} className="pl-sm-5">
-              <p className="bio mt-4">
-                Web Developer, with a marketing and hospitality manager
-                background. Full-stack certificate from Columbia University
-                during which I developed a comprehensive portfolio. My skillset
-                and experience, put me at the forefront to join an engineering
-                team tasked to accomplish strategic launches of polished user
-                friendly apps.
-                <br />
-                I’ve set sail my own segue from France to the USA, and more
-                recently from hospitality, with goals to achieve this transition
-                towards becoming an accomplished Software Engineer.
-                <br />
-                Having worked in different industries, I have a great ability to
-                balance distinct skill sets. I enjoy working in teams, am a
-                great listener and communicator, looking to join a team with
-                similar values and working on meaningful projects.
-                <br />
-                <span className="important">
-                  Ambition is my drive, learning new things is my fuel.
-                </span>
-                <br />
-                During my down time, I like watching documentaries, listening to
-                music and baking delicious Canelés from Bordeaux!
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
-  );
+//array of topics
+const topicsArray = [
+  "HTML",
+  "JavaScript",
+  "ReactJS",
+  "Cheese",
+  "jQuery",
+  "NodeJS",
+  "MySQL",
+  "Baguette",
+  "MongoDB",
+  "Bootstrap",
+  "MaterialUI",
+  "APIs",
+  "Express",
+];
+
+class About extends React.Component {
+  //state to start at index 0
+  state = { topicsIndex: 0 };
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      //current index is 0
+      let currentIdx = this.state.topicsIndex;
+      //state to go up by 1 index every 1.5 sec
+      this.setState({ topicsIndex: currentIdx + 1 });
+    }, 1200);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timeout);
+  }
+  render() {
+    let changeTopics = topicsArray[this.state.topicsIndex % topicsArray.length];
+    return (
+      <>
+        <NavBar />
+        <div className="aboutbody">
+          <Container className="container" style={{ marginTop: "10em" }}>
+            <Row>
+              <Col md={4} className="first-column text-center">
+                <Image
+                  className="french-cat"
+                  width="220"
+                  src="https://i.pinimg.com/736x/e6/3f/94/e63f94393ad1765dbd4923e17c94f140--oui-oui-berets.jpg"
+                  roundedCircle
+                />
+                <h4 className="contact mt-3">
+                  NEW YORK, NY 10010
+                  <br /> <span className="cell">(917) 847-1533</span>
+                  <br />
+                  <a
+                    data-toggle="tooltip"
+                    title="Send me a message!"
+                    id="email"
+                    href="mailto:1jessicaperez@gmail.com"
+                  >
+                    1JESSICAPEREZ@GMAIL.COM
+                  </a>
+                </h4>
+              </Col>
+              <Col md={8} className="pl-sm-5">
+                <h3 className="skills text-left">
+                  OUI OUI to <span id="topics-name">{changeTopics}</span>
+                </h3>
+                <p className="bio mt-4">
+                  Web Developer, with a marketing and hospitality manager
+                  background. Full-stack certificate from Columbia University
+                  during which I developed a comprehensive portfolio. My
+                  skillset and experience, put me at the forefront to join an
+                  engineering team tasked to accomplish strategic launches of
+                  polished user friendly apps.
+                  <br />
+                  I’ve set sail my own segue from France to the USA, and more
+                  recently from hospitality, with goals to achieve this
+                  transition towards becoming an accomplished Software Engineer.
+                  <br />
+                  Having worked in different industries, I have a great ability
+                  to balance distinct skill sets. I enjoy working in teams, am a
+                  great listener and communicator, looking to join a team with
+                  similar values and working on meaningful projects.
+                  <br />
+                  <span className="important">
+                    Ambition is my drive, learning new things is my fuel.
+                  </span>
+                  <br />
+                  During my down time, I like watching documentaries, listening
+                  to music and baking delicious Canelés from Bordeaux!
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
+  }
 }
-
 export default About;
