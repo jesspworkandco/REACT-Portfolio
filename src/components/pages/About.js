82f71d09277fc8d119aa4bdad2f240e0d75ed71f
Jessica Perez <1jessicaperez@gmail.com>
2020-10-23 18:16:05 -0400

    Bio updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,16 +10,12 @@ const topicsArray = [
   "HTML",
   "JavaScript",
   "ReactJS",
-  "Cheese",
   "jQuery",
   "NodeJS",
   "MySQL",
-  "Baguette",
   "MongoDB",
   "Bootstrap",
   "MaterialUI",
-  "APIs",
-  "Express",
 ];
 const topicStyle = {
   color: "#008080",
@@ -46,52 +42,24 @@ class About extends React.Component {
         <div className="aboutbody">
           <Container className="container" style={{ marginTop: "8em" }}>
             <Row>
-              <Col sm={0} md={2} className="first-column text-center">
-                {/* <Image
-                  className="french-cat"
-                  width="220"
-                  src="https://i.pinimg.com/736x/e6/3f/94/e63f94393ad1765dbd4923e17c94f140--oui-oui-berets.jpg"
-                  roundedCircle
-                /> */}
-                {/* <h4 className="contact mt-3">
-                  NEW YORK, NY 10010
-                  <br /> <span className="cell">(917) 847-1533</span>
-                  <br />
-                  <a
-                    data-toggle="tooltip"
-                    title="Send me a message!"
-                    id="email"
-                    href="mailto:1jessicaperez@gmail.com"
-                  >
-                    1JESSICAPEREZ@GMAIL.COM
-                  </a>
-                </h4> */}
-              </Col>
+              <Col sm={0} md={2} className="first-column text-center"></Col>
               <Col sm={12} md={10} className="pl-sm-5">
                 <h3 className="skills text-left">
-                  OUI OUI to: <span style={topicStyle}>{changeTopics}</span>
+                  Technical skills:{" "}
+                  <span style={topicStyle}>{changeTopics}</span>
                 </h3>
-                <h5>Aspiring Frontend Developer...</h5>
+                <h5>Enthusiastic Frontend Developer</h5>
                 <p className="bio mt-4">
-                  ...with a marketing and hospitality manager background.
-                  Full-stack certificate from Columbia University during which I
-                  developed a comprehensive portfolio. My skillset and
-                  experience, put me at the forefront to join an engineering
-                  team tasked to accomplish strategic launches of polished user
-                  friendly apps.
+                  Recent graduate from Columbia University, aspiring Frontent
+                  Developer with a marketing background.
                   <br />
-                  I’ve set sail my own segue from France to the USA, and more
-                  recently from hospitality, with goals to achieve this
-                  transition towards becoming an accomplished Software Engineer.
+                  Well organized with strong managing and client facing skills.
+                  Analytical, decisive, coupled with a passion for exceeding
+                  expectations. Passion for self-teaching and learning new
+                  skills quickly.
                   <br />
-                  Having worked in different industries, I have a great ability
-                  to balance distinct skill sets. I enjoy working in teams, am a
-                  great listener and communicator, looking to join a team with
-                  similar values and working on meaningful projects.
-                  <br />
-                  <span className="important">
-                    Ambition is my drive, learning new things is my fuel.
-                  </span>
+                  Having worked in different positions and industries, I can
+                  balance distinct skill sets and wear different hats.
                   <br />
                   During my down time, I like watching documentaries, listening
                   to music and baking delicious Canelés from Bordeaux!
